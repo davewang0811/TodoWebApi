4ef03f1e63374db7abb6113b103a2c19a7d9c0f8
Dave <dave.wang@tpisoftware.com>
2023-04-11 16:20:34 +0800

    Test 完成

---
+++ TestCoravel/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Coravel;
+
+namespace TestCoravel
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddScheduler();
+
+            services.BuildServiceProvider().UseScheduler(scheduler =>
+            {
+                scheduler.Schedule<DXAYJob>()
+                      .EveryMinute();
+                //.DailyAt(16, 0); // UTC Time(台灣凌晨0點)
+
+                var ctbcJobConfigSection = DateTime.Now;
+                var hour = ctbcJobConfigSection.Hour;
+                var minute = ctbcJobConfigSection.Minute;
+
+                scheduler.Schedule<T01Job>()
+                    .DailyAt(hour, minute); // UTC Time(台灣10點)
+
+                var t02Hour = SetHour(hour, minute, 30);
+                var t02Minute = SetMinute(minute, 30);
+
+                scheduler.Schedule<T02Job>()
+                    .DailyAt(t02Hour, t02Minute); // UTC Time(台灣10點) T01啟動時間+30
+
+                var t03Hour = SetHour(hour, minute, 40);
+                var t03Minute = SetMinute(minute, 40);
+
+                scheduler.Schedule<T03Job>()
+                    .DailyAt(t03Hour, t03Minute); // UTC Time(台灣10點) T01啟動時間+40
+
+                var t05Hour = SetHour(hour, minute, 50);
+                var t05Minute = SetMinute(minute, 50);
+
+                scheduler.Schedule<T05Job>()
+                    .DailyAt(t05Hour, t05Minute); // UTC Time(台灣10點) T01啟動時間+50
+
+            });
+        }
+    }
+}
---
+++ TestTask/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal class Program
+    {
+        class test
+        {
+            public int abdc { get; set; }
+            public string efg { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            string str1 = "NetFusion";
+            string str2 = "NETFSUSION";
+
+            if (str1 == str2)
+            {
+                Console.WriteLine("AA");
+            }
+            int[] MyFriend = { 1, 2, 3, 4, 5 };
+            int[] YourFriend = { 1, 5, 6, 7, 8 };
+            var OurFriend = MyFriend.Intersect(YourFriend).ToList();
+            OurFriend.ForEach(Console.WriteLine);
+
+            var ss = string.Equals(str1, str2, StringComparison.OrdinalIgnoreCase);
+
+            var pass = "MXFhejJ3c3ghUUFaQFdTWA==";
+            byte[] data = Convert.FromBase64String(pass);
+            pass = Encoding.UTF8.GetString(data);
+            Console.WriteLine(pass);
+
+        }
+
+        class C1
+        {
+            public string Data = "";
+        }
+        class C2
+        {
+            public string Data = "";
+            public string Test = "";
+            public static bool operator ==(C2 x, C2 y)
+            {
+                return x.Data == y.Data;
+            }
+            public static bool operator !=(C2 x, C2 y)
+            {
+                return x.Data != y.Data;
+            }
+        }
+    }
+}
